9c4df075d484e257c2b59362c7a523fa3d86ae1c
RacuSandu <sandu.racu2004@gmail.com>
2025-09-19 12:45:20 +0300

    Products Entity

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -42,6 +42,12 @@ export function MainNav({
             active: pathname === `/${params.storeId}/colors`,
         },
 
+         {
+            href: `/${params.storeId}/products`,
+            label: 'Products',
+            active: pathname === `/${params.storeId}/products`,
+        },
+
         {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
